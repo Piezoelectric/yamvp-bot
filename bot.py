38e0213eee6b99a884b932bfc7b0eed7a7a87068
Piezoelectric <23746623+Piezoelectric@users.noreply.github.com>
2023-12-17 15:19:27 -0600

    reorganize command cogs + integrate MVP scanning into bot

--- bot.py
+++ bot.py
@@ -1,14 +1,10 @@
 from discord.ext import tasks, commands
+from image_parse import ImageParser
 
 class DefaultCommands(commands.Cog):
-    def __init__(self, bot, ip, inv_l):
+    def __init__(self, bot, inv_l):
         self.bot = bot
-        self.index = 0
-        self.image_parser = ip
         self.invite_link = inv_l
-
-    def cog_unload(self):
-        self.printer.cancel()
 
     @commands.hybrid_command()
     async def test(self, ctx):
@@ -18,26 +14,37 @@ class DefaultCommands(commands.Cog):
     async def invite(self, ctx):
         await ctx.send(f'Invite me to your server: {self.invite_link}')
 
-    # == TASK-RELATED COMMANDS ==
-        
+class MvpCommands(commands.Cog):
+    def __init__(self, bot, filepath, bbox, wait_time):
+        self.bot = bot
+        self.image_parser = ImageParser(filepath, bbox)
+        self.mvp_messages = {}
+        self.wait_time = wait_time
+    
+    def cog_unload(self):
+        self.mvp_task.cancel()
+
+    # == COMMANDS FOR REGISTERING THE TASK TO A CHANNEL ==
+
     @commands.hybrid_command()
     async def start_loop(self, ctx):
         await ctx.send('Starting the loop and printing to this channel')
-        self.printer.start(ctx)
+        self.mvp_task.start(ctx)
 
     @commands.hybrid_command()
     async def cancel_loop(self, ctx):
         await ctx.send('Cancelling the loop')
-        self.printer.cancel()
+        self.mvp_task.cancel()
 
     # == THE ACTUAL TASK ==
 
     @tasks.loop(seconds=5.0)
-    async def printer(self, ctx):
-        await ctx.send(f'task test: {self.index}')
-        self.index += 1
+    async def mvp_task(self, ctx):
+        self.image_parser.capture()
+        output = self.image_parser.clean_and_parse()
+        await ctx.send(output)
 
-    @printer.before_loop
-    async def before_printer(self):
+    @mvp_task.before_loop
+    async def before_mvp_task(self):
         print('waiting to start task until bot is ready')
         await self.bot.wait_until_ready()
--- main.py
+++ main.py
@@ -3,26 +3,27 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 from image_parse import ImageParser
-from bot import DefaultCommands
+from bot import DefaultCommands, MvpCommands
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+load_dotenv()
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 INVITE_LINK = 'https://discord.com/api/oauth2/authorize?client_id=728785529679773758&permissions=2048&scope=bot+applications.commands'
 
-load_dotenv()
+default_commands_cog = DefaultCommands(bot, INVITE_LINK)
 
 TESSERACT_FILEPATH = os.getenv('TESSERACT_FILEPATH')
-BBOX = tuple(os.getenv('CAPTURE_REGION').split(','))
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+BBOX = tuple([int(a) for a in os.getenv('CAPTURE_REGION').split(',')])
 
-ip = ImageParser(TESSERACT_FILEPATH, BBOX)
-cog = DefaultCommands(bot, ip, INVITE_LINK)
+mvp_commands_cog = MvpCommands(bot, TESSERACT_FILEPATH, BBOX, 5)
 
 @bot.event
 async def on_ready():
-    await bot.add_cog(cog)
+    await bot.add_cog(default_commands_cog)
+    await bot.add_cog(mvp_commands_cog)
     synced = await bot.tree.sync()
     print(f'We have logged in as {bot.user}\n',
           f'synced commands {[c.name for c in synced]}\n',
