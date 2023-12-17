a873cc696177f6c1a64aa52bfc1d6b005f7e251c
Piezoelectric <23746623+Piezoelectric@users.noreply.github.com>
2023-12-17 14:27:46 -0600

    task example

--- bot.py
+++ bot.py
@@ -1,28 +1,41 @@
 import discord
-from discord import Interaction
-from discord.ext import commands
+from discord.ext import tasks, commands
 from dotenv import load_dotenv
 import os
 
 intents = discord.Intents.default()
 intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 INVITE_LINK = 'https://discord.com/api/oauth2/authorize?client_id=728785529679773758&permissions=2048&scope=bot+applications.commands'
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+class DefaultCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.index = 0
+        self.printer.start()
+
+    def cog_unload(self):
+        self.printer.cancel()
+
+    @commands.hybrid_command()
+    async def test(self, ctx):
+        await ctx.send("respondse")
+    
+    @commands.hybrid_command()
+    async def invite(self, ctx):
+        await ctx.send(f'Invite me to your server: {INVITE_LINK}')
+    
+    @tasks.loop(seconds=5.0)
+    async def printer(self):
+        print(f'task test: {self.index}')
+        self.index += 1
 
 @bot.event
 async def on_ready():
+    await bot.add_cog(DefaultCommands(bot))
     synced = await bot.tree.sync()
     print(f'We have logged in as {bot.user}, synced commands {[c.name for c in synced]}')
-
-@bot.hybrid_command()
-async def test(ctx):
-    await ctx.send("respondse")
-
-@bot.hybrid_command()
-async def invite(ctx):
-    await ctx.send(f'Invite me to your server: {INVITE_LINK}')
 
 if __name__ == '__main__':
     load_dotenv()
