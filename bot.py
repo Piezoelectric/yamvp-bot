aa9de0c107925d267c29bba9f860cef4d6bab5ed
Piezoelectric <23746623+Piezoelectric@users.noreply.github.com>
2023-12-17 14:45:12 -0600

    add task which prints to command context

--- bot.py
+++ bot.py
@@ -13,7 +13,6 @@ class DefaultCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.index = 0
-        self.printer.start()
 
     def cog_unload(self):
         self.printer.cancel()
@@ -25,11 +24,30 @@ class DefaultCommands(commands.Cog):
     @commands.hybrid_command()
     async def invite(self, ctx):
         await ctx.send(f'Invite me to your server: {INVITE_LINK}')
-    
+
+    # == TASK-RELATED COMMANDS ==
+        
+    @commands.hybrid_command()
+    async def start_loop(self, ctx):
+        await ctx.send('starting the loop')
+        self.printer.start(ctx)
+
+    @commands.hybrid_command()
+    async def cancel_loop(self, ctx):
+        await ctx.send('cancelling the loop')
+        self.printer.cancel()
+
+    # == THE ACTUAL TASK ==
+
     @tasks.loop(seconds=5.0)
-    async def printer(self):
-        print(f'task test: {self.index}')
+    async def printer(self, ctx):
+        await ctx.send(f'task test: {self.index}')
         self.index += 1
+
+    @printer.before_loop
+    async def before_printer(self):
+        print('waiting to start task until bot is ready')
+        await self.bot.wait_until_ready()
 
 @bot.event
 async def on_ready():
